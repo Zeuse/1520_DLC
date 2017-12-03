882a43108d9e2c437749225448ab1ab03d6e4a21
Zeuse <anandasurya1@gmail.com>
2017-12-03 20:38:04 +0200

    Add files via upload

---
+++ DLC_TF/mod.lua
@@ -0,0 +1,29 @@
+function data()
+return {
+
+	info = {
+		tags = { "Europe", "USSR", "Russia", "Ukraine", "Latvia", "Bulgaria", "Belarus" },
+
+		minorVersion = 1,
+		severityAdd = "NONE",
+		severityRemove = "NONE",
+		name = _("1520"),
+		description = _("test"),
+		authors = {
+		{
+			name = "Zeus",
+			role = "CREATOR",
+			
+
+		}
+  },
+		visible = true
+	},
+	options = {
+	},
+	
+	runFn = function (settings)
+		
+	end
+}
+end
